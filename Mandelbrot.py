1ed5dc4b673f7a385cae0e9c801b7d2d6fc71527
Varato <imxinchen@outlook.com>
2017-05-23 11:17:46 +0800

    update

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,12 +1,13 @@
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
-import itertools as it
-
 
 def C_Mat_gen(x0, y0, N, M, h):
 	'''
 	generates C matrix
+	x0, y0: lower left coordinates
+	N, M: dimensions of C matrix
+	h: step size
 	'''
 	C_mat = np.zeros([N, M, 2])
 	for i in range(N):
@@ -17,6 +18,7 @@ def C_Mat_gen(x0, y0, N, M, h):
 
 def iterate(C_mat):
 	'''
+	implements global iteration
 	C_mat: N*M*2 matrix
 	'''
 	passed = []
@@ -27,19 +29,21 @@ def iterate(C_mat):
 		for i in range(N):
 			for j in range(M):
 				z0 = Z_mat[i,j]
-				if (i,j) in passed:
-					continue
-				else:
-					Z_mat[i,j] = np.array([z0[0]**2-z0[1]**2, 2*z0[0]*z0[1]]) + C_mat[i,j]
-					if la.norm(Z_mat[i,j]) >= 2:
-						passed.append((i,j))
-						current_passed.append((i,j))
+				if (i,j) in passed: continue
+				Z_mat[i,j] = np.array([z0[0]**2-z0[1]**2, 2*z0[0]*z0[1]]) \
+					+ C_mat[i,j]
+				if la.norm(Z_mat[i,j]) >= 2:
+					passed.append((i,j))
+					current_passed.append((i,j))
 		yield current_passed
 
 def draw_map(ax, C_mat, n_max=3):
+	'''
+	draws Mandelbrot set after n_max iterations
+	'''
 	N, M, d = C_mat.shape
 	color_mat = np.zeros([N,M,3])
-	colors = it.cycle(plt.cm.rainbow(np.linspace(0,1,n_max)))
+	colors = iter(plt.cm.rainbow(np.linspace(0,1,n_max+1)))
 	c = next(colors)
 
 	k = 0
@@ -55,15 +59,13 @@ def draw_map(ax, C_mat, n_max=3):
 
 	ax.imshow(color_mat, interpolation="nearest")
 
-
-
 def main():
 	fig, ax = plt.subplots()
 	ax.set_xticks([])
 	ax.set_yticks([])
-	C_mat = C_Mat_gen(-2.2, -2.2, 90, 90, 0.05)
-	draw_map(ax, C_mat, n_max=10)
-	plt.savefig("n_max10.png", dpi=300)
+	C_mat = C_Mat_gen(x0 = -2.2, y0=-2.2, N = 90, M = 90, h = 0.05)
+	draw_map(ax, C_mat, n_max = 10)
+	plt.savefig("n_max10.png", dpi = 300)
 	plt.show()
 
 if __name__ == "__main__":
